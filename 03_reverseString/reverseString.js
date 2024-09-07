8c33e898de33621351d34aa6e7045afec93146be
Jofred <71094875+JofredG@users.noreply.github.com>
2024-09-07 17:25:12 -0500

    Completed JS-exercises 03-05

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let s='';
+  for(let i=string.length-1; i>=0; i--){
+    console.log(string.length);
+    s+= string[i];
+  }
+  return s;
 };
 
 // Do not edit below this line
--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+  if(a<0 || b<0) return "ERROR";
+  if(!Number.isInteger(a) || !Number.isInteger(b)) return "ERROR";
+  if(!Number.isFinite(a) || !Number.isFinite(b)) return "ERROR";
+  let min = Math.min(a,b);
+  let max = Math.max(a,b);
+  let sum = 0;
+  for (let i=min; i <= max; i++){
+    sum += i;
+  }
+  return sum;
 };
 
 // Do not edit below this line
